7942c5cd4cbb11bfadefcc5d8a15c667815a6390
Gabriel Ramalho <biel-pedro@hotmail.com>
2024-05-22 09:39:19 -0300

    feat: add error handler

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,55 @@
-import Fastify from 'fastify'
+import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 
-const app = Fastify()
+import { env } from '../env'
+import { errorHandler } from './error-handler'
 
-app.listen({ port: 3333 }).then(() => {
-  console.log('HTTP running 🔥')
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.setErrorHandler(errorHandler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Event Wind',
+      description: 'Organize your events and participants',
+      version: '1.0.0',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
+app.register(fastifyCors)
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`HTTP running 🔥 at http://localhost:${env.PORT}`)
 })
