f00fd22171074a8ce371ad4c48c2d7721e22b4e6
Gabriel Ramalho <biel-pedro@hotmail.com>
2024-05-22 14:16:28 -0300

    feat: add new authenticate route

--- src/http/server.ts
+++ src/http/server.ts
@@ -12,6 +12,7 @@ import {
 
 import { env } from '../env'
 import { errorHandler } from './error-handler'
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -52,6 +53,7 @@ app.register(fastifyJwt, {
 app.register(fastifyCors)
 
 app.register(createAccount)
+app.register(authenticateWithPassword)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`HTTP running 🔥 at http://localhost:${env.PORT}`)
